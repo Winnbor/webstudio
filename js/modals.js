3e9a8f193e19e460546564d47b6136184d40bb8f
Winnbor <59263951+Winnbor@users.noreply.github.com>
2021-05-13 20:11:20 +0300

    Refactor modal

--- js/modals.js
+++ js/modals.js
@@ -1,15 +1,15 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  backdrop: document.querySelector('[data-backdrop]'),
+  modal: document.querySelector('[data-modal]'),
+};
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.addEventListener('click', onToggleModal);
+refs.closeModalBtn.addEventListener('click', onToggleModal);
 
-  function toggleModal() {
-    document.body.classList.toggle('modal-open');
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+function onToggleModal() {
+  document.body.classList.toggle('modal-open');
+  document.body.classList.toggle('show-modal');
+  refs.modal.classList.toggle('is-visible');
+}
